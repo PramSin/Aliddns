789ca55bf9cbfbbe25be3473b47d90ebf3e33e45
PramSin <pram_sin@foxmail.com>
2022-07-11 13:57:54 +0800

    版本 2.1.0 | 添加对ipv6相关域名解析的支持

--- README.md
+++ README.md
@@ -1,32 +1,30 @@
-# aliddns 2.0.0
+# AliDDNS 2.1.0
 
-阿里云动态DDNS配置的 maven 项目，Java 版本为16.0.1。运行程序后，程序会自动获取当前主机公网ip地址与配置文件中主机名的解析记录，若解析记录与当前公网ip不匹配，则更改域名解析，否则不做更改。
+阿里云动态DDNS配置的 maven 项目，Java
+版本为16.0.1。运行程序后，程序会自动获取当前主机公网ip地址（支持ipv6）与配置文件中主机名的解析记录，若解析记录与当前公网ip不匹配，则更改域名解析，否则不做更改；若无法查询到ip地址，则删除相关解析设置。ip地址的获取由 [jsonip.com](https://jsonip.com)
+网站提供技术支持。
 
 本程序改编自 https://github.com/zngw/aliddns
 
-## 配置文件说明
+## 配置
 
 文件 config.json 如下所示
 
 ```json
 {
-  "regionId": "cn-shanghai",
-  "accessKeyId": "前面创建账号的AccessKeyID",
-  "secret": "前面创建账号的AccessKey Secret",
-  "tld": "xxx.xxx",
-  "rr": "xxx"
+   "regionId": "cn-shanghai",
+   "accessKeyId": "AccessKeyID",
+   "secret": "AccessKey Secret",
+   "tld": "xxx.xxx",
+   "rr": "xxx"
 }
 ```
 
 * regionId: 区域,域名管理一般是上海"cn-shanghai"
-* accessKeyId: 前面创建账号的AccessKeyID
-* secret: 前面创建账号的AccessKey Secret
+* accessKeyId: 对相应域名解析具有增删改查权限的账号的AccessKeyID
+* secret: 对相应域名解析具有增删改查权限的账号的AccessKey Secret
 * tld: 顶级域名
 * rr: 主机名
-
-## 注意
-
-请不要更改 pom.xml 文件中 `aliyun-java-sdk-core` 的版本，否则会由于它使用的 `gson` 版本与 `aliyun-java-sdk-alidns` 不同而报错。
 
 ## 启动
 
@@ -35,3 +33,7 @@
    文件的路径 `java -jar /your/path/for/jar/file/aliddns-2.0.0.jar /your/path/for/json/file/config.json`
 
 可以使用`crontab`来定时进行动态域名解析。
+
+## 注意
+
+请不要更改 pom.xml 文件中 `aliyun-java-sdk-core` 的版本，否则会由于它使用的 `gson` 版本与 `aliyun-java-sdk-alidns` 不同而报错。
--- src/main/java/com/pram/aliddns/DDNS.java
+++ src/main/java/com/pram/aliddns/DDNS.java
@@ -3,10 +3,7 @@ package com.pram.aliddns;
 import com.alibaba.fastjson.JSON;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
-import com.aliyuncs.alidns.model.v20150109.DescribeSubDomainRecordsRequest;
-import com.aliyuncs.alidns.model.v20150109.DescribeSubDomainRecordsResponse;
-import com.aliyuncs.alidns.model.v20150109.UpdateDomainRecordRequest;
-import com.aliyuncs.alidns.model.v20150109.UpdateDomainRecordResponse;
+import com.aliyuncs.alidns.model.v20150109.*;
 import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.profile.DefaultProfile;
 
@@ -14,6 +11,7 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -30,20 +28,28 @@ public class DDNS {
     private String host = null;         // 完整域名
     private Config cfg = null;          // 配置文件
 
+    public static void main(String[] args) {
+        String filename = (args.length == 0) ? "./config.json" : args[0];
+        DDNS ddns = new DDNS();
+        String[] ip = ddns.init(filename);
+        String ipv4 = ip[0];
+        String ipv6 = ip[1];
+        ddns.run(ipv4, ipv6);
+    }
+
     /**
      * 初始化
      */
-    private void init(String filename) {
+    private String[] init(String filename) {
         Date date = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println("当前时间：" + dateFormat.format(date));
-
+        System.out.println("\n----- 当前时间：" + dateFormat.format(date) + " -----\n");
 
         String cfgStr = readJsonFile(filename);
         cfg = JSON.parseObject(cfgStr, Config.class);
         if (cfg == null) {
             System.out.println("读取配置文件" + filename + "失败");
-            return;
+            return new String[]{"", ""};
         }
         cfg.dnsInterval *= 1000;
         cfg.ipInterval *= 1000;
@@ -54,21 +60,16 @@ public class DDNS {
             cfg.rr = "@";
             host = cfg.tld;
         }
-        System.out.println("本机ip：" + getCurrenHostIp() + "\n主机名 " + host + " 解析中...");
+        return new String[]{getCurrenHostIpv4(), getCurrenHostIpv6()};
     }
 
-    /**
-     * 获取当前主机公网IP
-     */
-    private String getCurrenHostIp() {
-        // 这里使用jsonip.com第三方接口获取本地IP
-        String jsonip = "https://jsonip.com";
+    private StringBuilder getJsonResponse(String json_ip) {
         // 接口返回结果
         StringBuilder result = new StringBuilder();
         BufferedReader in = null;
         try {
             // 使用HttpURLConnection网络请求第三方接口
-            URL url = new URL(jsonip);
+            URL url = new URL(json_ip);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("GET");
             urlConnection.connect();
@@ -78,7 +79,7 @@ public class DDNS {
                 result.append(line);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            return new StringBuilder();
         }
         // 使用finally块来关闭输入流
         finally {
@@ -90,9 +91,21 @@ public class DDNS {
                 e2.printStackTrace();
             }
         }
+        return result;
+    }
+
+    /**
+     * 获取当前主机公网IP
+     */
+    private String getCurrenHostIpv4() {
+        // 这里使用jsonip.com第三方接口获取本地IP
+        String jsonip = "https://ipv4.jsonip.com/";
+
+        StringBuilder result = getJsonResponse(jsonip);
+
         //  正则表达式，提取xxx.xxx.xxx.xxx，将IP地址从接口返回结果中提取出来
-        String rexp = "(\\d{1,3}\\.){3}\\d{1,3}";
-        Pattern pat = Pattern.compile(rexp);
+        String rex = "(\\d{1,3}\\.){3}\\d{1,3}";
+        Pattern pat = Pattern.compile(rex);
         Matcher mat = pat.matcher(result.toString());
         String res = "";
         if (mat.find()) {
@@ -115,24 +128,128 @@ public class DDNS {
         }
     }
 
+    private String getCurrenHostIpv6() {
+        // 这里使用jsonip.com第三方接口获取本地IP
+        String jsonip = "https://ipv6.jsonip.com/";
+
+        StringBuilder result = getJsonResponse(jsonip);
+
+        //  正则表达式，提取xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx，将IP地址从接口返回结果中提取出来
+        String rex = "([\\da-fA-F]{1,4}:){7}[\\da-fA-F]{1,4}";
+        Pattern pat = Pattern.compile(rex);
+        Matcher mat = pat.matcher(result.toString());
+        String res = "";
+        if (mat.find()) {
+            res = mat.group();
+        }
+        return res;
+    }
+
     /**
-     * 修改解析记录
+     * 添加DNS解析记录
      */
-    private UpdateDomainRecordResponse updateDomainRecord(UpdateDomainRecordRequest request, IAcsClient client) {
+    private void addDNSRecord(IAcsClient client, String ip, Boolean is_v6) {
+        //  修改解析记录
+        AddDomainRecordRequest addDomainRecordRequest = new AddDomainRecordRequest();
+        //  域名名称
+        addDomainRecordRequest.setDomainName(host);
+        //  主机记录
+        addDomainRecordRequest.setRR(cfg.rr);
+        //  将主机记录值改为当前主机IP
+        addDomainRecordRequest.setValue(ip);
+        //  解析记录类型
+        if (is_v6) {
+            addDomainRecordRequest.setType("AAAA");
+            System.out.println("\n----添加ipv6解析地址----");
+        } else {
+            addDomainRecordRequest.setType("A");
+            System.out.println("\n----添加ipv4解析地址----");
+        }
+
         try {
             //  调用SDK发送请求
-            return client.getAcsResponse(request);
+            client.getAcsResponse(addDomainRecordRequest);
         } catch (ClientException e) {
             e.printStackTrace();
             //  发生调用错误，抛出运行时异常
             throw new RuntimeException();
         }
+
+        System.out.println("主机名 " + host + " 解析地址添加:" + ip + "\n");
     }
 
     /**
-     * 检测IP是否改变，改变了就修改
+     * 修改DNS解析记录
      */
-    private void check(String ip) {
+    private void updateDNSRecord(DescribeSubDomainRecordsResponse.Record record, IAcsClient client, String ip, Boolean is_v6) {
+        //  记录ID
+        String recordId = record.getRecordId();
+        //  记录值
+        // 域名IP
+        String recordsIp = record.getValue();
+
+        if (!ip.equals(recordsIp)) {
+            //  修改解析记录
+            UpdateDomainRecordRequest updateDomainRecordRequest = new UpdateDomainRecordRequest();
+            //  主机记录
+            updateDomainRecordRequest.setRR(cfg.rr);
+            //  记录ID
+            updateDomainRecordRequest.setRecordId(recordId);
+            //  将主机记录值改为当前主机IP
+            updateDomainRecordRequest.setValue(ip);
+            //  解析记录类型
+            if (is_v6) {
+                updateDomainRecordRequest.setType("AAAA");
+                System.out.println("\n----更新ipv6解析地址----");
+            } else {
+                updateDomainRecordRequest.setType("A");
+                System.out.println("\n----更新ipv4解析地址----");
+            }
+
+            try {
+                //  调用SDK发送请求
+                client.getAcsResponse(updateDomainRecordRequest);
+            } catch (ClientException e) {
+                e.printStackTrace();
+                //  发生调用错误，抛出运行时异常
+                throw new RuntimeException();
+            }
+
+            System.out.println("主机名 " + host + " 解析地址已修改为:" + ip + "\n");
+
+        } else {
+            System.out.println("主机名 " + host + " 解析记录与当前主机公网ip " + ip + " 相同，无需修改解析地址\n");
+        }
+    }
+
+    private void deleteDNSRecord(DescribeSubDomainRecordsResponse.Record record, IAcsClient client, Boolean is_v6) {
+        //  修改解析记录
+        DeleteDomainRecordRequest deleteDomainRecordRequest = new DeleteDomainRecordRequest();
+        //  记录ID
+        deleteDomainRecordRequest.setRecordId(record.getRecordId());
+        if (is_v6) {
+            System.out.println("\n----删除ipv6解析地址----");
+        } else {
+            System.out.println("\n----删除ipv4解析地址----");
+        }
+
+        try {
+            //  调用SDK发送请求
+            client.getAcsResponse(deleteDomainRecordRequest);
+        } catch (ClientException e) {
+            e.printStackTrace();
+            //  发生调用错误，抛出运行时异常
+            throw new RuntimeException();
+        }
+
+        System.out.println("主机名 " + host + " 解析地址已删除\n");
+    }
+
+    /**
+     * 检测IP状态与解析记录，并进行解析记录的增删改
+     */
+    private void check(String ipv4, String ipv6) {
+
         //  设置鉴权参数，初始化客户端
         DefaultProfile profile;
         profile = DefaultProfile.getProfile(cfg.regionId, cfg.accessKeyId, cfg.secret);
@@ -142,49 +259,56 @@ public class DDNS {
         DescribeSubDomainRecordsRequest describeSubDomainRecordsRequest = new DescribeSubDomainRecordsRequest();
         describeSubDomainRecordsRequest.setSubDomain(host);
         DescribeSubDomainRecordsResponse describeSubDomainRecordsResponse = describeSubDomainRecords(describeSubDomainRecordsRequest, client);
+
         List<DescribeSubDomainRecordsResponse.Record> domainRecords = describeSubDomainRecordsResponse.getDomainRecords();
-        //最新的一条解析记录
-        if (domainRecords.size() != 0) {
-            DescribeSubDomainRecordsResponse.Record record = domainRecords.get(0);
-            //  记录ID
-            String recordId = record.getRecordId();
-            //  记录值
-            // 域名IP
-            String recordsIp = record.getValue();
 
-            if (!ip.equals(recordsIp)) {
-                //  修改解析记录
-                UpdateDomainRecordRequest updateDomainRecordRequest = new UpdateDomainRecordRequest();
-                //  主机记录
-                updateDomainRecordRequest.setRR(cfg.rr);
-                //  记录ID
-                updateDomainRecordRequest.setRecordId(recordId);
-                //  将主机记录值改为当前主机IP
-                updateDomainRecordRequest.setValue(ip);
-                //  解析记录类型
-                updateDomainRecordRequest.setType("A");
-                UpdateDomainRecordResponse updateDomainRecordResponse = updateDomainRecord(updateDomainRecordRequest, client);
+        DescribeSubDomainRecordsResponse.Record v4Record = null;
+        DescribeSubDomainRecordsResponse.Record v6Record = null;
+        for (DescribeSubDomainRecordsResponse.Record record :
+                domainRecords) {
 
-                System.out.println("主机名 " + host + " 解析地址已修改为:" + ip);
+            if (record.getType().equals("A")) {
+                v4Record = record;
+            } else if (record.getType().equals("AAAA")) {
+                v6Record = record;
+            }
+        }
 
-            } else {
-                System.out.println("主机名 " + host + " 解析记录与当前主机公网ip " + ip + " 相同，无需修改解析地址");
+        if (ipv4.equals("")) {
+            System.out.println("本机ipv4无法获取！\n主机名 " + host + " 解析中...");
+            if (v4Record != null) {
+                deleteDNSRecord(v4Record, client, false);
             }
         } else {
-            System.out.println("没有找到主机名 " + host + " 的解析记录");
+            System.out.println("本机ipv4：" + ipv4 + "\n主机名 " + host + " 解析中...");
+            if (v4Record != null) {
+                updateDNSRecord(v4Record, client, ipv4, false);
+            } else {
+                addDNSRecord(client, ipv4, false);
+            }
+        }
+        if (ipv6.equals("")) {
+            System.out.println("本机ipv6无法获取！\n主机名 " + host + " 解析中...");
+            if (v6Record != null) {
+                deleteDNSRecord(v6Record, client, true);
+            }
+        } else {
+            System.out.println("本机ipv6：" + ipv6 + "\n主机名 " + host + " 解析中...");
+            if (v6Record != null) {
+                updateDNSRecord(v6Record, client, ipv6, true);
+            } else {
+                addDNSRecord(client, ipv6, true);
+            }
         }
     }
 
     /**
      * 运行检测
      */
-    private void run() {
-        //  当前主机公网IP
-        String ip = getCurrenHostIp();
-
+    private void run(String ipv4, String ipv6) {
         while (true) {
             try {
-                check(ip);
+                check(ipv4, ipv6);
                 break;
             } catch (Exception e) {
                 e.printStackTrace();
@@ -202,7 +326,7 @@ public class DDNS {
             File jsonFile = new File(fileName);
             FileReader fileReader = new FileReader(jsonFile);
 
-            Reader reader = new InputStreamReader(new FileInputStream(jsonFile), StandardCharsets.UTF_8);
+            Reader reader = new InputStreamReader(Files.newInputStream(jsonFile.toPath()), StandardCharsets.UTF_8);
             int ch;
             StringBuilder sb = new StringBuilder();
             while ((ch = reader.read()) != -1) {
@@ -217,12 +341,5 @@ public class DDNS {
             e.printStackTrace();
             return null;
         }
-    }
-
-    public static void main(String[] args) {
-        String filename = (args.length == 0) ? "./config.json" : args[0];
-        DDNS ddns = new DDNS();
-        ddns.init(filename);
-        ddns.run();
     }
 }
